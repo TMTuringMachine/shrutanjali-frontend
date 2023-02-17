cb822eca75eedba1368ebc005e2334152dba63c5
shivam256 <shivamgavandi256@gmail.com>
2023-02-17 17:08:43 +0530

    SW-1 testing github jira connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
-    <h1 className="text-3xl mt-6 font-bold text-center">
-      Shrutanjali Website Home Page
-    </h1>
-  );  
+    <div>
+      <h1 className="text-3xl mt-6 font-bold text-center">
+        Shrutanjali Website Home Page
+      </h1>
+      <h1>SW1-TEST</h1>
+    </div>
+  );
 }
 
 export default App;
